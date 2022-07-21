239ae41c5333493b3f7154bcbdf6a4e8f728741d
Abhishek <you@example.com>
2022-07-21 20:10:34 +0530

    [Abhishek]: ensure player gets exact winning 100 added

---
+++ UC5_Ensure_rpeatation.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.snakeAndLadder;
+
+	public class UC5_Ensure_rpeatation extends UC3_player_option {
+		/*Ensure the player gets
+to exact winning
+position 100.*/
+		
+		static final int NOPLAY = 0, SNAKE = 1, LADDER = 2;
+		
+		public static void main(String[] args) {
+			int position = new UC1_getPosition().position;
+			
+			while(position < 100) {
+				int diceroll = getDiceRoll();
+				switch(getOption()) {
+					case NOPLAY:
+						position = position;
+						break;
+					case SNAKE:
+						position -= diceroll;
+						break;
+					case LADDER:
+						position += diceroll;
+						break;	
+				}
+				
+				if(position < 0) {
+					position = 0;
+				}
+				else if(position > 100 ) {
+					position -= diceroll;
+				}
+				System.out.println("position = "+position);	
+			}	
+		}
+}
